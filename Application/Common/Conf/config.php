c26a143cdf4b4046e55e4649bec86768f3d65b2f
xiaoluo <1370177585@qq.com>
2021-07-28 15:05:10 +0800

    first commit

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => 'localhost',
+	'DB_NAME'   => 'test1',
+	'DB_USER'   => 'root',
+	'DB_PWD'    => 'root',
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => '', // 数据库表前缀
+	'DB_CHARSET'=> 'utf8', // 字符集
+	'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
+	"URL_HTML_SUFFIX" => '',//url伪静态后缀
+	'URL_ROUTER_ON'   => true, 
+	'URL_ROUTE_RULES'=>array(
+	    ':id\d'               => 'Home/Item/Show?item_id=:1',
+	),
+	'URL_CASE_INSENSITIVE'=>true
+);
